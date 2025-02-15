7bc956dc7a31c616dc25b9fa2e39b8adb4c8d7c3
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-02-16 02:29:55 +0530

    image resources in use

--- Test Multi Platform/composeApp/src/commonMain/kotlin/org/example/test_multi/App.kt
+++ Test Multi Platform/composeApp/src/commonMain/kotlin/org/example/test_multi/App.kt
@@ -14,14 +14,43 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import testmultiplatform.composeapp.generated.resources.Res
 import testmultiplatform.composeapp.generated.resources.compose_multiplatform
+import testmultiplatform.composeapp.generated.resources.egypt
+import testmultiplatform.composeapp.generated.resources.france
+import testmultiplatform.composeapp.generated.resources.indonesia
+import testmultiplatform.composeapp.generated.resources.japan
+import testmultiplatform.composeapp.generated.resources.mexico
 
 @Composable
 @Preview
 fun App() {
+    val list = listOf(
+        ImageCombo(
+            name = "egypt",
+            res = Res.drawable.egypt
+        ),
+        ImageCombo(
+            name = "france",
+            res = Res.drawable.france
+        ),
+        ImageCombo(
+            name = "indonesia",
+            res = Res.drawable.indonesia
+        ),
+        ImageCombo(
+            name = "japan",
+            res = Res.drawable.japan
+        ),
+        ImageCombo(
+            name = "mexico",
+            res = Res.drawable.mexico
+        )
+    )
+
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
@@ -38,6 +67,20 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
+            val index = remember { mutableStateOf(0) }
+
+            Image(
+                painter = painterResource(resource = list.get(index = index.value).res),
+                contentDescription = null
+            )
+
+            Button(
+                onClick = {
+                    index.value = index.value.inc().takeUnless { it > list.lastIndex } ?: 0
+                },
+                content = { Text(text = "Switch Country") }
+            )
+
         }
     }
 }
@@ -53,4 +96,11 @@ expect fun getCar(): Car
 
 expect object FunctionCollections {
     fun testFunctionOne()
+}
+
+class ImageCombo(
+    val name: String,
+    val res: DrawableResource
+) {
+
 }
